a125e5004bf5c6d4d2cc950e711c74231517fff2
hery4we <henry4we2008@gmail.com>
2025-03-06 13:49:09 +0000

    Add product status filtering and enhance layout structure

--- surelock-app/src/pages/index.tsx
+++ surelock-app/src/pages/index.tsx
@@ -2,13 +2,19 @@ import PerProductCard from "@/components/global/per-product-card";
 import ProductCardLoader from "@/components/loaders/product-card-loader";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-// import { DEFAULT_URL } from "@/lib/constants";
 import { SERVER_URL } from "../lib/constants";
-import { ProductType } from "@/lib/types";
+import { ProductStatusType, ProductType } from "@/lib/types";
 import { SlidersHorizontal } from "lucide-react";
 import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
     const [loading, setLoading] = useState(false);
@@ -17,6 +23,67 @@ export default function Home() {
         null
     );
     const [input, setInput] = useState<string>("");
+
+
+
+    const [status, setStatus] = useState<ProductStatusType>("all");
+    const [minPrice, setMinPrice] = useState<string>("");
+    const [maxPrice, setMaxPrice] = useState<string>("");
+    const [filtersSet, setFiltersSet] = useState<boolean>(false);
+    const handleFilters = useCallback(() => {
+        setLoading(true);
+        const floatMinPrice = minPrice.length ? parseFloat(minPrice) : 0;
+        const floatMaxPrice = maxPrice.length ? parseFloat(maxPrice) : 0;
+        const maxPriceValue =
+            floatMaxPrice < floatMinPrice ? undefined : floatMaxPrice;
+        const filteredPrice =
+            floatMinPrice < 1
+                ? undefined
+                : `${floatMinPrice}${maxPriceValue ? "," + maxPriceValue : ""}`;
+
+        const filteredStatus =
+            status === "all" ? undefined : status === "active" ? true : false;
+
+        fetch(
+            `${SERVER_URL}?${
+                typeof filteredStatus !== "undefined"
+                    ? `is_active=` + filteredStatus
+                    : ""
+            }
+            &${filteredPrice ? `price=` + filteredPrice : ""}`,
+            {
+                headers: {
+                    Authorization:
+                        "Beearer " + process.env.NEXT_PUBLIC_API_KEY || "",
+                },
+            }
+        )
+            .then(res => res.json())
+            .then((data: ProductType[]) => {
+                setLoading(false);
+                if (!data || !data.length)
+                    return toast.error(`No results found`);
+                setFiltersSet(true);
+                console.log("New Data", data);
+                setSearchResults(data);
+            })
+            .catch(err => console.error(err));
+    }, [status, minPrice, maxPrice]);
+
+
+
+
+
+
+
+
+// commmememmme
+
+
+
+
+
+
 
     const handleSearch = useCallback(() => {
         if (input.length < 3)
@@ -57,7 +124,7 @@ export default function Home() {
             })
             .catch(err => console.error(err));
     }, []);
-
+    console.log("min price", minPrice);
     return (
         <div className="p-4 lg:p-10">
             <div className="mb-6">
@@ -90,11 +157,98 @@ export default function Home() {
                         />
                         <Button onClick={handleSearch}>Search</Button>
                     </div>
-                    <div className="py-2">
+                    {/* <div className="py-2">
                         <Button className="flex items-center" disabled>
                             <SlidersHorizontal />
                             Apply Filters
-                        </Button>
+                        </Button> */}
+                         <div className="py-2 flex justify-between w-full flex-1 flex-col md:flex-row gap-y-2">
+                        <div className="flex-1 px-2 flex gap-x-4 items-center">
+                            <div className="flex gap-x-4">
+                                <div>
+                                    <span className="text-supporting-text text-xs">
+                                        Min Price
+                                    </span>
+                                    <Input
+                                        value={minPrice}
+                                        onChange={e =>
+                                            setMinPrice(e.target.value)
+                                        }
+                                        placeholder="0.00"
+                                    />
+                                </div>
+                                <div>
+                                    <span className="text-supporting-text text-xs">
+                                        Max Price
+                                    </span>
+                                    <Input
+                                        value={maxPrice}
+                                        onChange={e =>
+                                            setMaxPrice(e.target.value)
+                                        }
+                                        placeholder="0.00"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="text-supporting-text text-xs">
+                                    Status
+                                </span>
+                                <Select
+                                    onValueChange={v => {
+                                        if (v === "all") {
+                                            setStatus("all");
+                                            return setSearchResults(null);
+                                        }
+                                        setStatus(v as ProductStatusType);
+                                    }}
+                                    defaultValue={status}
+                                    disabled={loading}
+                                >
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select the new status" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="all">All</SelectItem>
+                                        <SelectItem value="active">
+                                            Active
+                                        </SelectItem>
+                                        <SelectItem value="inactive">
+                                            Inactive
+                                        </SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-x-2">
+                            <Button
+                                className="flex items-center"
+                                disabled={
+                                    status === "all" && minPrice.length < 1
+                                }
+                                onClick={handleFilters}
+                            >
+                                <SlidersHorizontal />
+                                Apply Filters
+                            </Button>
+                            {filtersSet ? (
+                                <Button
+                                    className="flex items-center"
+                                    variant={"outline"}
+                                    onClick={() => {
+                                        setFiltersSet(false);
+                                        setMinPrice("");
+                                        setMaxPrice("");
+                                        setStatus("all");
+                                        setInput("");
+                                        setSearchResults(null);
+                                    }}
+                                >
+                                    Clear Filters
+                                </Button>
+                            ) : null}
+                        </div>
                     </div>
                 </div>
             </div>
